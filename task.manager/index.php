878e3bd3461f70e6c1728e66b4765d7e805c071c
Coremotor <coremotor@users.noreply.github.com>
2020-05-25 22:45:34 +0300

    task.manager

--- task.manager/index.php
+++ task.manager/index.php
@@ -1,3 +1,24 @@
+<pre>
+<?php
+    include "include/success.php";
+
+    $login = "admin";
+
+    $password = "pass";
+
+//    var_dump("GET", $_GET);
+//    var_dump("POST", $_POST);
+
+    if (isset($_POST["btnSend"])) {
+        if ($_POST["login"] == $login && $_POST["password"] == $password) {
+            $authorization = true;
+        } else {
+            $repeat = true;
+        }
+    }
+?>
+</pre>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +27,7 @@
     <title>Project - ведение списков</title>
 </head>
 
-<body>
+<body style="margin: 0 auto; width: 800px">
 
     <div class="header">
     	<div class="logo"><img src="img/logo.png" width="68" height="23" alt="Project"></div>
@@ -29,47 +50,55 @@
 			
 				<h1>Возможности проекта —</h1>
 				<p>Вести свои личные списки, например покупки в магазине, цели, задачи и многое другое. Делится списками с друзьями и просматривать списки друзей.</p>
-				
-			
-			</td>
-            <td class="right-collum-index">
-				
-				<div class="project-folders-menu">
-					<ul class="project-folders-v">
-    					<li class="project-folders-v-active"><a href="#">Авторизация</a></li>
-    					<li><a href="#">Регистрация</a></li>
-    					<li><a href="#">Забыли пароль?</a></li>
-					</ul>
-				    <div class="clearfix"></div>
-				</div>
-                
-				<div class="index-auth">
-                    <form action="" method="">
-						<table width="100%" border="0" cellspacing="0" cellpadding="0">
-							<tr>
-								<td class="iat">
-                                    <label for="login_id">Ваш e-mail:</label>
-                                    <input id="login_id" size="30" name="login">
-                                </td>
-							</tr>
-							<tr>
-								<td class="iat">
-                                    <label for="password_id">Ваш пароль:</label>
-                                    <input id="password_id" size="30" name="password" type="password">
-                                </td>
-							</tr>
-							<tr>
-								<td><input type="submit" value="Войти"></td>
-							</tr>
-						</table>
-                    </form>
-				</div>
-			
+
 			</td>
         </tr>
+
+
+        <tr>
+            <td class="right-collum-index <?=$_GET["login"] == "yes" ? "" : "display-none"?>">
+
+                <div class="project-folders-menu">
+                    <ul class="project-folders-v">
+                        <li class="project-folders-v-active"><a href="#">Авторизация</a></li>
+                        <li><a href="#">Регистрация</a></li>
+                        <li><a href="#">Забыли пароль?</a></li>
+                    </ul>
+                    <div class="clearfix"></div>
+                </div>
+
+                <div class="index-auth">
+                    <div style="margin: 20px" class=<?= $authorization ? "" : "display-none"?>><?=$massage?></div>
+
+                    <div style="margin: 20px" class=<?= $repeat ? "" : "display-none"?>>Неверный email или пароль</div>
+
+                    <form class=<?= $authorization ? "display-none" : ""?> action="index.php?login=yes" method="post">
+                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td class="iat">
+                                    <label for="login_id">Ваш e-mail:</label>
+                                    <input id="login_id" size="30" name="login" value="<?=isset($_POST["login"]) ? $_POST["login"] : ""?>">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="iat">
+                                    <label for="password_id">Ваш пароль:</label>
+                                    <input id="password_id" size="30" name="password" type="password" value="<?=isset($_POST["password"]) ? $_POST["password"] : ""?>">
+                                </td>
+                            </tr>
+                            <tr>
+                                <td><input type="submit" name="btnSend" "Войти"></td>
+                            </tr>
+                        </table>
+                    </form>
+                </div>
+
+            </td>
+        </tr>
+
     </table>
     
-    <div class="clearfix">
+    <div style="margin-bottom: 10px" class="clearfix">
         <ul class="main-menu bottom">
             <li><a href="#">Главная</a></li>
             <li><a href="#">О нас</a></li>
@@ -78,6 +107,8 @@
             <li><a href="#">Каталог</a></li>
         </ul>
     </div>
+
+    <a class="clearfix" style="color: white; font-size: 20px; margin: 10px" href="/?login=yes">Войти</a>
 
     <div class="footer">&copy;&nbsp;<nobr>2018</nobr> Project.</div>
 
